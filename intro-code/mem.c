a69e3da6224f9e8847013182c2acebac6b5ae593
Shivaram Venkataraman <shivaram@cs.wisc.edu>
2023-01-24 16:56:39 -0600

    Add code used in introduction lecture

---
+++ intro-code/mem.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "common.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) { 
+      fprintf(stderr, "usage: mem <value>\n"); 
+      exit(1); 
+    } 
+    int *p;                   // memory for pointer is on "stack"
+    p = malloc(sizeof(int));  // malloc'd memory is on "heap"
+    assert(p != NULL);
+    printf("(pid:%d) addr of p:        %llx\n", (int) getpid(), (unsigned long long) &p);
+    printf("(pid:%d) addr stored in p: %llx\n", (int) getpid(), (unsigned long long) p);
+    *p = atoi(argv[1]); // assign value to addr stored in p
+    while (1) {
+      Spin(1);
+      *p = *p + 1;
+      printf("(pid:%d) value of p: %d\n", getpid(), *p);
+    }
+
+    return 0;
+}
---
+++ intro-code/threads.v1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+
+pthread_mutex_t m;
+volatile int counter = 0; 
+int loops;
+
+void *worker(void *arg) {
+    int i;
+    for (i = 0; i < loops; i++) {
+      Pthread_mutex_lock(&m);
+      counter++;
+      Pthread_mutex_unlock(&m);
+    }
+    pthread_exit(NULL);
+}
+
+int
+main(int argc, char *argv[])
+{
+    if (argc != 2) { 
+      fprintf(stderr, "usage: threads <value>\n"); 
+      exit(1); 
+    } 
+    loops = atoi(argv[1]);
+    pthread_t p1, p2;
+    printf("Initial value : %d\n", counter);
+    Pthread_mutex_init(&m, NULL);
+    Pthread_create(&p1, NULL, worker, NULL); 
+    Pthread_create(&p2, NULL, worker, NULL);
+    Pthread_join(p1, NULL);
+    Pthread_join(p2, NULL);
+    printf("Final value   : %d\n", counter);
+    return 0;
+}
