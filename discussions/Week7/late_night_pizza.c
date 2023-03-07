304561e625d58b99b73c362b748d5b7c41fea9dd
Anjali05 <anjali@wisc.edu>
2023-03-07 15:39:16 -0600

    bug fix pizza

--- discussions/Week7/late_night_pizza.c
+++ discussions/Week7/late_night_pizza.c
@@ -1,18 +1,14 @@
 
 /*
 This implementation will keep producing and consuming pizza. 
-EXercise: Modify the solution to run it for n iterations.
+Exercise: Modify the solution to run it for n iterations.
 */
 
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
 
-//shared variable
-int slices = 0;
-
-int S = 1;
-int first = 1, have_pizza = 0;
 
 pthread_cond_t order = PTHREAD_COND_INITIALIZER;
 pthread_cond_t deliver =  PTHREAD_COND_INITIALIZER;
@@ -26,11 +22,18 @@ void prepare_pizza(){
     printf("Pizza is being prepared\n");
 }
 
+//shared variable
+volatile int slices = 0;
+
+int S = 2;
+volatile int first = 1; 
 
 void *pizza_delivery(){
-    while(1){
+    while(1){        
         pthread_mutex_lock(&mutex);
-        pthread_cond_wait(&order, &mutex);
+        while(first == 1){
+            pthread_cond_wait(&order, &mutex);
+        }
         prepare_pizza();
         slices = S;
         first = 1;
@@ -41,23 +44,19 @@ void *pizza_delivery(){
 
 void *student(){
     while(1){
-        pthread_mutex_lock(&mutex);       
-        while(!have_pizza){
-            if(slices > 0){
-                slices --;
-                have_pizza = 1;
+        pthread_mutex_lock(&mutex);  
+        while(slices == 0){
+
+            if(first){
+                first = 0;
+                pthread_cond_signal(&order);
             }
-            else{
-                if(first){
-                    pthread_cond_signal(&order);
-                    first = 0;
-                }
-                pthread_cond_wait(&deliver, &mutex);
-            }
+            pthread_cond_wait(&deliver, &mutex);
         }
+
+        slices--;
         pthread_mutex_unlock(&mutex);
         study();
-        have_pizza = 0;
     }
 }
 
@@ -66,8 +65,8 @@ int main(int argc, char * argv[]){
     pthread_create(&s, NULL, student, NULL);
     pthread_create(&d, NULL, pizza_delivery, NULL);
 
-    pthread_join(d, NULL);
     pthread_join(s, NULL);
+    pthread_join(d, NULL);
 
     return 0;
 }
